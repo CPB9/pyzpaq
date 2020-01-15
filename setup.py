d37a3d9f0c7f0d5faf30b00d472acd4ade54ac35
Sergey Kartashev <sergey.kartashev@sputnix.ru>
2020-01-15 16:03:49 +0300

    workaround for sources deploy

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 import sys
 from setuptools import setup, Extension
 
+ss = ['pyzpaq.cpp', 'zpaq/libzpaq.cpp']
+if sys.argv[1] == 'sdist':
+    ss += ['zpaq/libzpaq.h']
+
+print(ss)    
+
 zpaq = Extension('zpaq',
-                  sources = ['pyzpaq.cpp', 'zpaq/libzpaq.cpp'],
+                  sources = ss,
                   libraries = ['advapi32'] if sys.platform.startswith("win") else [])
 
 setup (name = 'pyzpaq',
-       version = '0.1.2',
+       version = '0.1.5',
        description = 'Python zpaq bindings',
        url='https://github.com/CPB9/pyzpaq.git',
        ext_modules = [zpaq])
